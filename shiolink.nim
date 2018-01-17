d457220137ac1c71d5775f6e04df28aef09d901b
Narazaka <info@narazaka.net>
2018-01-18 02:04:40 +0900

    現行のSHIOLINKプロトコルを最低限実装

--- shiolink.nim
+++ shiolink.nim
@@ -3,12 +3,17 @@ import shioridll
 import nuuid # not secure random
 import parsecfg
 import osproc
+import streams
+import strutils
 
 var config: Config
 var commandline: string
 var charmode: string
 var timeout: string
 var friendlyerror: string
+var shioriProcess: Process
+var shioriStdin: Stream
+var shioriStdout: Stream
 
 shioriLoadCallback = proc (dirpath: string): bool =
     config = loadConfig("shiolink.ini")
@@ -16,14 +21,34 @@ shioriLoadCallback = proc (dirpath: string): bool =
     charmode = config.getSectionValue("SHIOLINK", "charmode")
     timeout = config.getSectionValue("SHIOLINK", "timeout")
     friendlyerror = config.getSectionValue("SHIOLINK", "friendlyerror")
-    let uuid: string = generateUUID()
-    echo uuid
+
+    var commandTokens = commandline.split(" ") # TODO: arg parse
+    shioriProcess = startProcess(commandTokens[0], ".", commandTokens[1..^1]) # TODO: config
+    shioriStdin = shioriProcess.inputStream
+    shioriStdout = shioriProcess.outputStream
+
+    shioriStdin.writeLine("*L:" & dirpath)
     true
 
 shioriRequestCallback = proc (requestStr: string): string =
-    return $newResponse(SHIORI, "3.0", Status.OK)
+    let uuid: string = generateUUID()
+    let sendCommandLine = "*S:" & uuid
+    shioriStdin.writeLine(sendCommandLine)
+    let recvCommandLine = shioriStdout.readLine()
+    if sendCommandLine != recvCommandLine:
+        raise newException(ValueError, "invalid return line")
+    shioriStdin.write(requestStr)
+    var line: string = ""
+    var lines: seq[string] = @[]
+    while shioriStdout.readLine(line):
+        lines.add(line)
+        if line.len() == 0:
+            break
+    return lines.join("\n")
 
 shioriUnloadCallback = proc (): bool =
+    shioriStdin.writeLine("*U:")
+    shioriProcess.close()
     true
 
 when appType != "lib":
